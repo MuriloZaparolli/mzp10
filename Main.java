de219dca7fa2907367ad66467ae95d805f47a930
Murilo de Lima Zaparolli <muzapa10.mzp10@gmail.com>
2025-07-29 14:14:43 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ public class Main {
     final static Scanner SC = new Scanner(System.in);
 
     public static void main(String[] args) {
-        float[][] matriz = new float[12][12];
-        float soma=0;
+        double[][] matriz = new double[12][12];
+        double soma=0;
         int cnt=0;
         char caractere;
         caractere = SC.next().charAt(0);
         for (int linha=0; linha<matriz.length; linha++) {
             for (int coluna=0; coluna<matriz.length; coluna++) {
-                matriz[linha][coluna]=SC.nextFloat();
-                if (linha<coluna) {
+                matriz[linha][coluna]=SC.nextDouble();
+                if (linha+coluna>11 && linha<coluna) {
                     soma+=matriz[linha][coluna];
                     cnt++;
                 }
